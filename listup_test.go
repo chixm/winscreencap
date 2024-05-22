8b6ddf50e120c21c5ef6b018e4800e696227b207
Chixm <makoto0072104@yahoo.co.jp>
2024-05-22 20:57:56 +0900

    add multiple window test

--- listup_test.go
+++ listup_test.go
@@ -1,15 +1,55 @@
 package winscreencap_test
 
 import (
+	"fmt"
+	"image/png"
 	"log"
+	"os"
 	"testing"
 
 	"github.com/chixm/winscreencap"
+	"github.com/lxn/win"
 )
 
 func TestListUpWindow(t *testing.T) {
 	windowList := winscreencap.GetWindowList()
 	for _, v := range windowList {
 		log.Println(v.Title, v.Hwnd)
+	}
+}
+
+// This test will make screenshot files for each window in your PC
+// Browser window such as GoogleChrome, Edge black outs screenshot for maybe security reason.
+func TestListUpAndTakeShot(t *testing.T) {
+	var index int
+
+	windowList := winscreencap.GetWindowList()
+	for _, v := range windowList {
+		// only visible window
+		if !win.IsWindowVisible(v.Hwnd) {
+			continue
+		}
+
+		index++
+
+		img, err := winscreencap.CaptureWindow(v.Hwnd)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		log.Println(v.Title+` as screenshot No`, index)
+
+		file, err := os.Create(fmt.Sprintf("screenshot%d.png", index))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer file.Close()
+
+		err = png.Encode(file, img)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
 	}
 }
